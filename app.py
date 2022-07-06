965a68e751c45fd510f9ede379ebfad7a93481d3
ecarmona1992 <emanuelcarmona714@gmail.com>
2022-07-05 17:03:33 -0700

    fixed app.py to pass test

--- app.py
+++ app.py
@@ -1,23 +1,9 @@
-# from flask import Flask
-# import os
-
-# app = Flask(__name__)
-
-
-# @app.route('/')
-# def home():
-#     return 'Hello World!!!'
-
-
-# if __name__ == "__main__":
-#     port = int(os.environ.get('PORT', 5000))
-#     app.run(debug=True, host='0.0.0.0', port=port)
 from flask import Flask
 app = Flask(__name__)
 import os
 @app.route('/')
 def hello():
-    return 'Hello World bunk!\n'
+    return 'Hello World!\n'
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
