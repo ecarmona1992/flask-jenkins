e16528af726016e300888ec9a665fb0906a0f794
ecarmona1992 <emanuelcarmona714@gmail.com>
2022-07-12 12:58:48 -0700

    testing k8s change

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 import os
 @app.route('/')
 def hello():
-    return 'This is a new change that gets added to kubernetes\n'
+    return 'Testing out a second change\n'
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
